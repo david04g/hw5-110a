1edd15f5f64147d00e58fba0ab29342fe9cc2847
david04g <dgarc148@ucsc.edu>
2025-06-11 15:47:22 +0000

    new change

--- local_value_numbering.py
+++ local_value_numbering.py
@@ -1,53 +1,70 @@
 import re
+from typing import List, Tuple
 
-def is_virtual(var):
+def is_virtual(var: str) -> bool:
     return var.startswith("vr") or var.startswith("_new_name")
 
-def parse_instruction(instr):
+# Patterns for matching ClassIeR instructions
+_op_pat   = re.compile(r"(\S+)\s*=\s*(\S+)\((\S+),\s*(\S+)\)")
+_copy_pat = re.compile(r"(\S+)\s*=\s*(\S+)")
+
+def parse_instruction(instr: str) -> Tuple[str, ...]:
     instr = instr.strip()
-    m = re.match(r"(\S+)\s*=\s*(\S+)\((\S+),\s*(\S+)\)", instr)
+    m = _op_pat.match(instr)
     if m:
-        dest, op, arg1, arg2 = m.groups()
-        return ('op', dest, op, arg1, arg2)
-    m = re.match(r"(\S+)\s*=\s*(\S+)", instr)
+        dst, op, a1, a2 = m.groups()
+        return ('op', dst, op, a1, a2)
+    m = _copy_pat.match(instr)
     if m:
-        dest, src = m.groups()
-        return ('copy', dest, src)
+        dst, src = m.groups()
+        return ('copy', dst, src)
     return ('other', instr)
 
-def LVN(program):
-    expr_table = {}
-    value_table = {}
-    new_program = []
-    new_vars = set()
-    replaced = 0
+def LVN(program: List[str]) -> Tuple[List[str], List[str], int]:
+    """
+    Perform local value numbering:
+    - program: list of ClassIeR code lines
+    Returns:
+      1. new program list with arithmetic redundancies replaced by copies
+      2. list of new virtual registers used
+      3. count of replaced arithmetic instructions
+    """
+    expr_table   = {}             # maps (op, operands) -> existing dest
+    new_program  = []             # output lines
+    new_vars     = set()          # collect any new regs
+    replaced_cnt = 0
 
     for line in program:
         kind = parse_instruction(line)
 
         if kind[0] == 'op':
-            _, dest, op, arg1, arg2 = kind
-            if not (is_virtual(arg1) and is_virtual(arg2)):
-                new_program.append(line)
+            _, dst, op, a1, a2 = kind
+            # only virtual registers
+            if is_virtual(a1) and is_virtual(a2):
+                # commutative ops
+                if op in ('add','mul','eq'):
+                    key = (op, tuple(sorted((a1,a2))))
+                else:
+                    key = (op, (a1,a2))
+
+                if key in expr_table:
+                    existing = expr_table[key]
+                    new_program.append(f"{dst} = {existing}")
+                    replaced_cnt += 1
+                else:
+                    expr_table[key] = dst
+                    new_program.append(line)
+                new_vars.add(dst)
                 continue
-
-            key = (op, tuple(sorted([arg1, arg2])) if op in ['add', 'mul', 'eq'] else (arg1, arg2))
-            if key in expr_table:
-                existing = expr_table[key]
-                new_program.append(f"{dest} = {existing}")
-                replaced += 1
-            else:
-                expr_table[key] = dest
-                new_program.append(line)
-            new_vars.add(dest)
-
-        elif kind[0] == 'copy':
-            _, dest, src = kind
+        if kind[0] == 'copy':
+            _, dst, src = kind
             new_program.append(line)
-            if is_virtual(dest):
-                new_vars.add(dest)
+            if is_virtual(dst):
+                new_vars.add(dst)
+            continue
 
-        else:
-            new_program.append(line)
+        # all other instructions
+        new_program.append(line)
 
-    return new_program, sorted(list(new_vars)), replaced
+    return new_program, sorted(new_vars), replaced_cnt
+
--- parser.py
+++ parser.py
@@ -1,4 +1,3 @@
-
 import pdb
 import class_ast as class_ast
 from class_ast import *
@@ -6,15 +5,21 @@ from typing import Callable, List, Tuple, Optional
 from scanner import Lexeme, Token, Scanner
 from enum import Enum
 
+# Extra classes:
+
+# Keeps track of the type of an ID,
+# i.e. whether it is a program variable
+# or an IO variable
 class IDType(Enum):
     IO = 1
     VAR = 2
 
+# The data to be stored for each ID in the symbol table
 class SymbolTableData:
     def __init__(self, id_type: IDType, data_type: Type, new_name: str) -> None:
-        self.id_type = id_type
-        self.data_type = data_type
-        self.new_name = new_name
+        self.id_type = id_type      # IO or VAR
+        self.data_type = data_type  # INT or FLOAT
+        self.new_name = new_name    # renaming for scoping
 
     def get_id_type(self) -> IDType:
         return self.id_type
@@ -25,11 +30,14 @@ class SymbolTableData:
     def get_new_name(self) -> str:
         return self.new_name
 
+# Symbol Table exception, requires a line number and ID
 class SymbolTableException(Exception):
     def __init__(self, lineno: int, ID: str) -> None:
-        message = "Symbol table error on line: " + str(lineno) + "\nUndeclared ID: " + str(ID)
+        message = "Symbol table error on line: " + str(lineno) + \
+                  "\nUndeclared ID: " + str(ID)
         super().__init__(message)
 
+# Generates new labels
 class NewLabelGenerator():
     def __init__(self) -> None:
         self.counter = 0
@@ -39,6 +47,7 @@ class NewLabelGenerator():
         self.counter += 1
         return new_label
 
+# Generates new names for program variables
 class NewNameGenerator():
     def __init__(self) -> None:
         self.counter = 0
@@ -50,6 +59,7 @@ class NewNameGenerator():
         self.new_names.append(new_name)
         return new_name
 
+# Allocates virtual registers
 class VRAllocator():
     def __init__(self) -> None:
         self.counter = 0
@@ -62,9 +72,10 @@ class VRAllocator():
     def declare_variables(self) -> List[str]:
         ret = []
         for i in range(self.counter):
-            ret.append("virtual_reg vr%d;" % i)
+            ret.append(f"virtual_reg vr{i};")
         return ret
 
+# Symbol table class
 class SymbolTable:
     def __init__(self) -> None:
         self.ht_stack = [dict()]
@@ -73,7 +84,7 @@ class SymbolTable:
         info = SymbolTableData(id_type, data_type, ID)
         self.ht_stack[-1][ID] = info
 
-    def lookup(self, ID: str) -> Optional:
+    def lookup(self, ID: str) -> Optional[SymbolTableData]:
         for ht in reversed(self.ht_stack):
             if ID in ht:
                 return ht[ID]
@@ -85,12 +96,17 @@ class SymbolTable:
     def pop_scope(self) -> None:
         self.ht_stack.pop()
 
+# Parser Exception
 class ParserException(Exception):
     def __init__(self, lineno: int, lexeme: Lexeme, tokens: List[Token]) -> None:
-        message = "Parser error on line: " + str(lineno) + "\nExpected one of: " + str(tokens) + "\nGot: " + str(lexeme)
+        message = ("Parser error on line: " + str(lineno) +
+                   "\nExpected one of: " + str(tokens) +
+                   "\nGot: " + str(lexeme))
         super().__init__(message)
 
+# Parser class
 class Parser:
+
     def __init__(self, scanner: Scanner) -> None:
         self.scanner = scanner
         self.symbol_table = SymbolTable()
@@ -98,8 +114,8 @@ class Parser:
         self.nlg = NewLabelGenerator()
         self.nng = NewNameGenerator()
         self.function_name = None
-        self.function_args = []
-        self.unroll_factor = 1  # default, override externally if needed
+        self.function_args: List[Tuple[str,str]] = []
+        self.unroll_factor = 1
 
     def parse(self, s: str) -> List[str]:
         self.scanner.input_string(s)
@@ -116,7 +132,9 @@ class Parser:
     def eat(self, check: Token) -> None:
         token_id = self.get_token_id(self.to_match)
         if token_id != check:
-            raise ParserException(self.scanner.get_lineno(), self.to_match, [check])
+            raise ParserException(self.scanner.get_lineno(),
+                                  self.to_match,
+                                  [check])
         self.to_match = self.scanner.token()
 
     def parse_function(self) -> List[str]:
@@ -132,11 +150,11 @@ class Parser:
         self.eat(Token.ID)
         self.eat(Token.LPAR)
         self.function_name = function_name
-        args = self.parse_arg_list()
+        args = self.parse_arg_list() or []
         self.function_args = args
         self.eat(Token.RPAR)
 
-    def parse_arg_list(self) -> List[Tuple[str, str]]:
+    def parse_arg_list(self) -> List[Tuple[str,str]]:
         token_id = self.get_token_id(self.to_match)
         if token_id == Token.RPAR:
             return []
@@ -145,10 +163,9 @@ class Parser:
         if token_id == Token.RPAR:
             return [arg]
         self.eat(Token.COMMA)
-        arg_l = self.parse_arg_list()
-        return arg_l + [arg]
+        return self.parse_arg_list() + [arg]
 
-    def parse_arg(self) -> Tuple[str, str]:
+    def parse_arg(self) -> Tuple[str,str]:
         token_id = self.get_token_id(self.to_match)
         if token_id == Token.FLOAT:
             self.eat(Token.FLOAT)
@@ -159,61 +176,276 @@ class Parser:
             data_type = Type.INT
             data_type_str = "int"
         else:
-            raise ParserException(self.scanner.get_lineno(), self.to_match, [Token.INT, Token.FLOAT])
+            raise ParserException(self.scanner.get_lineno(),
+                                  self.to_match,
+                                  [Token.INT, Token.FLOAT])
         self.eat(Token.AMP)
         id_name = self.to_match.value
         self.eat(Token.ID)
         self.symbol_table.insert(id_name, IDType.IO, data_type)
         return (id_name, data_type_str)
 
-def parse_for_statement(self) -> List[str]:
-    self.eat(Token.FOR)
-    self.eat(Token.LPAR)
+    def parse_statement_list(self) -> List[str]:
+        token_id = self.get_token_id(self.to_match)
+        if token_id in [Token.INT, Token.FLOAT, Token.ID, Token.IF, Token.LBRACE, Token.FOR]:
+            stm = self.parse_statement() or []
+            return stm + self.parse_statement_list()
+        return []
 
-    # Parse init assignment
-    init_code = self.parse_assignment_statement()
-    init_assign = init_code[0] if init_code else ""
-    self.eat(Token.SEMI)
+    def parse_statement(self) -> List[str]:
+        token_id = self.get_token_id(self.to_match)
+        if token_id in [Token.INT, Token.FLOAT]:
+            return self.parse_declaration_statement() or []
+        if token_id == Token.ID:
+            return self.parse_assignment_statement() or []
+        if token_id == Token.IF:
+            return self.parse_if_else_statement() or []
+        if token_id == Token.LBRACE:
+            return self.parse_block_statement() or []
+        if token_id == Token.FOR:
+            return self.parse_for_statement() or []
+        raise ParserException(self.scanner.get_lineno(),
+                              self.to_match,
+                              [Token.FOR, Token.IF, Token.LBRACE,
+                               Token.INT, Token.FLOAT, Token.ID])
 
-    # Parse condition
-    cond_ast = self.parse_expr()
-    self.eat(Token.SEMI)
+    def parse_declaration_statement(self) -> List[str]:
+        token_id = self.get_token_id(self.to_match)
+        if token_id == Token.INT:
+            self.eat(Token.INT)
+            id_name = self.to_match.value
+            self.eat(Token.ID)
+            self.symbol_table.insert(id_name, IDType.VAR, Type.INT)
+            self.eat(Token.SEMI)
+            return []
+        if token_id == Token.FLOAT:
+            self.eat(Token.FLOAT)
+            id_name = self.to_match.value
+            self.eat(Token.ID)
+            self.symbol_table.insert(id_name, IDType.VAR, Type.FLOAT)
+            self.eat(Token.SEMI)
+            return []
+        raise ParserException(self.scanner.get_lineno(),
+                              self.to_match,
+                              [Token.INT, Token.FLOAT])
 
-    # Parse update assignment
-    update_code = self.parse_assignment_statement_base()
-    update_assign = update_code[0] if update_code else ""
-    self.eat(Token.RPAR)
+    def parse_assignment_statement(self) -> List[str]:
+        base = self.parse_assignment_statement_base()
+        self.eat(Token.SEMI)
+        return base or []
 
-    # Parse body
-    self.symbol_table.push_scope()
-    body_code = self.parse_statement()
-    self.symbol_table.pop_scope()
+    def parse_assignment_statement_base(self) -> List[str]:
+        id_name = self.to_match.value
+        id_data = self.symbol_table.lookup(id_name)
+        if id_data is None:
+            raise SymbolTableException(self.scanner.get_lineno(), id_name)
+        self.eat(Token.ID)
+        self.eat(Token.ASSIGN)
+        self.parse_expr()
+        return []
 
-    # Attempt unrolling if pattern matches
-    try:
-        if (cond_ast.op == '<' and isinstance(cond_ast.left, ASTIDNode) and isinstance(cond_ast.right, ASTNUMNode)):
-            loop_var = cond_ast.left.name
-            limit = cond_ast.right.value
+    def parse_if_else_statement(self) -> List[str]:
+        self.eat(Token.IF)
+        self.eat(Token.LPAR)
+        self.parse_expr()
+        self.eat(Token.RPAR)
+        self.parse_statement()
+        self.eat(Token.ELSE)
+        self.parse_statement()
+        return []
 
-            if isinstance(init_assign, str) and f"{loop_var}=" in init_assign:
-                start_val = int(init_assign.split('=')[1].strip())
-                if isinstance(update_assign, str) and update_assign.strip() == f"{loop_var}={loop_var}+1":
-                    unroll_factor = getattr(self, 'unroll_factor', 1)
-                    trip_count = limit - start_val
-                    if trip_count % unroll_factor == 0:
-                        result = []
-                        result.append(init_assign)
-                        for i in range(start_val, limit, unroll_factor):
-                            for j in range(unroll_factor):
-                                iter_val = i + j
-                                for stmt in body_code:
-                                    result.append(stmt.replace(loop_var, str(iter_val)))
-                        return result
-    except:
-        pass
+    def parse_block_statement(self) -> List[str]:
+        self.eat(Token.LBRACE)
+        self.symbol_table.push_scope()
+        body = self.parse_statement_list()
+        self.symbol_table.pop_scope()
+        self.eat(Token.RBRACE)
+        return []
 
-    # Fallback if not unrolled
-    label_start = self.nlg.mk_new_label()
-    label_end = self.nlg.mk_new_label()
-    cond_code = ["ifFalse " + cond_ast.to_code() + " goto " + label_end]
-    return init_code + [label_start + ":"] + cond_code + body_code + update_code + ["goto " + label_start, label_end + ":"]
+    def parse_for_statement(self) -> List[str]:
+        self.eat(Token.FOR)
+        self.eat(Token.LPAR)
+
+        # initializer
+        init_code = self.parse_assignment_statement()
+        self.eat(Token.SEMI)
+
+        # condition
+        cond_ast = self.parse_expr()
+        self.eat(Token.SEMI)
+
+        # update
+        update_code = self.parse_assignment_statement_base()
+        self.eat(Token.RPAR)
+
+        # body
+        self.symbol_table.push_scope()
+        body_code = self.parse_statement() or []
+        self.symbol_table.pop_scope()
+
+        # loop unrolling
+        try:
+            if (isinstance(cond_ast, ASTBinOpNode)
+                and cond_ast.op == '<'
+                and isinstance(cond_ast.left, ASTIDNode)
+                and isinstance(cond_ast.right, ASTNUMNode)):
+
+                var = cond_ast.left.name
+                limit = cond_ast.right.value
+
+                init_str = init_code[0].strip() if len(init_code)==1 else ""
+                upd_str  = update_code[0].strip() if len(update_code)==1 else ""
+
+                if init_str.startswith(f"{var}=") and upd_str == f"{var}={var}+1":
+                    start = int(init_str.split('=')[1])
+                    uf = self.unroll_factor
+                    count = limit - start
+                    if count % uf == 0:
+                        out = []
+                        for v in range(start, limit, uf):
+                            for j in range(uf):
+                                iv = v + j
+                                for ln in body_code:
+                                    out.append(ln.replace(var, str(iv)))
+                        return out
+        except:
+            pass
+
+        # fallback
+        L0 = self.nlg.mk_new_label()
+        L1 = self.nlg.mk_new_label()
+        cond_line = f"ifFalse {cond_ast.to_code()} goto {L1}"
+        return init_code + [f"{L0}:"] + [cond_line] + body_code + update_code + [f"goto {L0}", f"{L1}:"]
+# you need to build and return an AST
+    def parse_expr(self) -> ASTNode:        
+        self.parse_comp()
+        self.parse_expr2()
+        return
+
+    # you need to build and return an AST
+    def parse_expr2(self) -> ASTNode:
+        token_id = self.get_token_id(self.to_match)
+        if token_id in [Token.EQ]:
+            self.eat(Token.EQ)
+            self.parse_comp()
+            self.parse_expr2()
+            return
+        if token_id in [Token.SEMI, Token.RPAR]:
+            return 
+        
+        raise ParserException(self.scanner.get_lineno(),
+                              self.to_match,            
+                              [Token.EQ, Token.SEMI, Token.RPAR])
+    
+    # you need to build and return an AST
+    def parse_comp(self) -> ASTNode:
+        self.parse_factor()
+        self.parse_comp2()
+        return
+
+    # you need to build and return an AST
+    def parse_comp2(self) -> ASTNode:
+        token_id = self.get_token_id(self.to_match)
+        if token_id in [Token.LT]:
+            self.eat(Token.LT)
+            self.parse_factor()
+            self.parse_comp2()
+            return
+        if token_id in [Token.SEMI, Token.RPAR, Token.EQ]:
+            return 
+        
+        raise ParserException(self.scanner.get_lineno(),
+                              self.to_match,            
+                              [Token.EQ, Token.SEMI, Token.RPAR, Token.LT])
+
+    # you need to build and return an AST
+    def parse_factor(self) -> ASTNode:
+        self.parse_term()
+        self.parse_factor2()
+        return
+
+    # you need to build and return an AST
+    def parse_factor2(self) -> ASTNode:
+        token_id = self.get_token_id(self.to_match)
+        if token_id in [Token.PLUS]:
+            self.eat(Token.PLUS)
+            self.parse_term()            
+            self.parse_factor2()
+            return
+        if token_id in [Token.MINUS]:
+            self.eat(Token.MINUS)
+            self.parse_term()
+            self.parse_factor2()
+            return
+        if token_id in [Token.EQ, Token.SEMI, Token.RPAR, Token.LT]:
+            return
+
+        raise ParserException(self.scanner.get_lineno(),
+                              self.to_match,            
+                              [Token.EQ, Token.SEMI, Token.RPAR, Token.LT, Token.PLUS, Token.MINUS])
+    
+    # you need to build and return an AST
+    def parse_term(self) -> ASTNode:
+        self.parse_unit()
+        self.parse_term2()
+        return
+
+    # you need to build and return an AST
+    def parse_term2(self) -> ASTNode:
+        token_id = self.get_token_id(self.to_match)
+        if token_id in [Token.DIV]:
+            self.eat(Token.DIV)
+            self.parse_unit()
+            self.parse_term2()
+            return 
+        if token_id in [Token.MUL]:
+            self.eat(Token.MUL)
+            self.parse_unit()
+            self.parse_term2()
+            return 
+        if token_id in [Token.EQ, Token.SEMI, Token.RPAR, Token.LT, Token.PLUS, Token.MINUS]:
+            return 
+
+        raise ParserException(self.scanner.get_lineno(),
+                              self.to_match,            
+                              [Token.EQ, Token.SEMI, Token.RPAR, Token.LT, Token.PLUS, Token.MINUS, Token.MUL, Token.DIV])
+
+    # you need to build and return an AST
+    def parse_unit(self) -> ASTNode:
+        token_id = self.get_token_id(self.to_match)
+        if token_id in [Token.NUM]:
+            self.eat(Token.NUM)            
+            return
+        if token_id in [Token.ID]:
+            id_name = self.to_match.value
+            id_data = self.symbol_table.lookup(id_name)
+            if id_data == None:
+                raise SymbolTableException(self.scanner.get_lineno(), id_name)
+            self.eat(Token.ID)
+            return
+        if token_id in [Token.LPAR]:
+            self.eat(Token.LPAR)
+            self.parse_expr()
+            self.eat(Token.RPAR)
+            return
+            
+        raise ParserException(self.scanner.get_lineno(),
+                              self.to_match,            
+                              [Token.NUM, Token.ID, Token.LPAR])    
+
+# Type inference start
+
+# I suggest making functions like this to check
+# what class a node belongs to.
+def is_leaf_node(node) -> bool:
+    return issubclass(type(node), ASTLeafNode)
+
+# Type inference top level
+def type_inference(node) -> Type:
+    
+    if is_leaf_node(node):
+        return node.node_type
+    
+    # next check if it is a unary op, then a bin op.
+    # remember that comparison operators (eq and lt)
+    # are handled a bit differently
